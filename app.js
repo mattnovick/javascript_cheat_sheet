9e42baf5157cf8b7d1aebf4b724c304cd1a06942
matt novick <mattnovick@outlook.com>
2015-12-17 01:15:31 -0500

    Updated assignment cheat sheet. Just need to clarify testing and add Logic section.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function addNums(){
-   var num1 = document.getElementById('num1').value;   
-   var num2 = document.getElementById('num2').value;   
-   document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2)); 
-}
+// function addNums(){
+//    var num1 = document.getElementById('num1').value;   
+//    var num2 = document.getElementById('num2').value;   
+//    document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2)); 
+// }
 
 
 
@@ -11,15 +11,49 @@ function addNums(){
 
 
 
+
+
+
+
+
+
+// Variables
 
 var x = 4
 var y = 5
 console.log(4 + 5);
 
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+
+// ways of declaring variables
+var person = "John Doe", carName = "Volvo", price = 200;
+var person = "John Doe",
+carName = "Volvo",
+price = 200;
+
+
+// Alerts
+alert("I'm an alert box!");
 
 var peanuts = ["Snoopy", "Charlie Brown"];
 var muppets = ["Kermit", "Big Bird"];
 alert(peanuts + "," + muppets);
+
+
+
+
+
+
+//Output
+Writing into an alert box, using window.alert().
+Writing into the HTML output using document.write().
+Writing into an HTML element, using innerHTML.
+Writing into the browser console, using console.log().
+
+
+
 
 var user = {firstName: 'Matthew', lastName: 'Novick', birthday: '11/15/91', interests: 'scuba'};
 console.log(user.firstName, user.lastName, user.birthday, user.interests);
