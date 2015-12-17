b2e25ea20487f7b10ca1501d78d723bf8250b219
matt novick <mattnovick@outlook.com>
2015-12-17 00:47:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+function addNums(){
+   var num1 = document.getElementById('num1').value;   
+   var num2 = document.getElementById('num2').value;   
+   document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2)); 
+}
+
+
+
+
+
+
+
+
+
+var x = 4
+var y = 5
+console.log(4 + 5);
+
+
+var peanuts = ["Snoopy", "Charlie Brown"];
+var muppets = ["Kermit", "Big Bird"];
+alert(peanuts + "," + muppets);
+
+var user = {firstName: 'Matthew', lastName: 'Novick', birthday: '11/15/91', interests: 'scuba'};
+console.log(user.firstName, user.lastName, user.birthday, user.interests);
+
+
+
