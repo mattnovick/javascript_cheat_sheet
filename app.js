1f1033e6608bff5bce92b10ccf6366e4b8581ea7
matt novick <mattnovick@outlook.com>
2015-12-22 13:20:36 -0500

    added function examples to javascript cheat sheet

--- app.js
+++ app.js
@@ -113,17 +113,79 @@ console.log("You will see this though, since 5 < 10 evaluates to false");
 // Logic
 // Operators
 ==        equals to                                 x==8
-===      equal value and equal type        x===5
-!=         not equal                                  x ! = 8
-!==       not equal value or type              x! == 8
+===      equal value and equal type        			x===5
+!=         not equal                                x ! = 8
+!==       not equal value or type              		x! == 8
 ><        greater than, less than
 >= <=   greather than or equal/less
-&&        and                                           (x < 10 && y > 1) is true
+&&        and                                       (x < 10 && y > 1) is true
 ||       or                                   (x == 5 || y == 5) is false
 !        not                                 !(x == y) is true
 
 var user = {firstName: 'Matthew', lastName: 'Novick', birthday: '11/15/91', interests: 'scuba'};
 console.log(user.firstName, user.lastName, user.birthday, user.interests);
+
+
+
+
+
+
+// FUNCTIONS
+// declare the function
+function describePerson(name, adjective) {
+  return alert(name + " is " + adjective);
+}
+
+// invoke the function
+describePerson("Zach", "cool");
+describePerson("Smalltalk", "furry");
+
+
+function sayHello(name) {
+  return alert("hello " + name);
+} 
+
+sayHello("john");
+
+// blank function
+function saySomething() {
+  return console.log("alksdfjlksdjflkasjdf");
+}
+saySomething();
+
+
+// Prize door example
+function openDoor(door) {
+  if (door === 1) {
+    return alert("you won a pizza");
+  } else if (door === 2) {
+    return alert("you won a coffee");
+  } else {
+    return alert("you win absolutely nothing");
+  }
+}
+openDoor(1000);
+
+
+
+// Another example
+var name1 = "matt";
+var name2 = "tod";
+
+function namessame(name) {
+	if (name1 === name2) {
+	alert("names are the same")
+	}
+	else {
+	alert("names aren't the same")
+	}
+}
+
+// Another Example with numbers
+function someName(numberOne, otherNumber){   
+return numberOne + 10 + otherNumber; }
+console console.log(someName(4, 14)); // Calling a function is also known as "invoking" it 
+>28
 
 
 
